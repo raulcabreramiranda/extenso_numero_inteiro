ce62f91cf2b8f52dcd64dc3f60f5a67ccf90aeb5
Raul Cabrera Miranda <raul@qsforweb.com.br>
2019-04-09 17:31:09 -0300

    Dockerizando a aplicação

--- run.py
+++ run.py
@@ -2,18 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from app import api_bp
 
-def create_app(config_filename):
+def create_app():
     app = Flask(__name__)
     app.config['JSONIFY_MIMETYPE'] = 'application/ld+json'
     CORS(app)
-    app.config.from_object(config_filename)
-
-
     app.register_blueprint(api_bp, url_prefix='/')
 
     return app
 
-
 if __name__ == "__main__":
-    app = create_app("config")
-    app.run()
+    app = create_app()
+    app.run(host='0.0.0.0')
